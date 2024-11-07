ef1e01bf07e6dd753a906b58430a03486a07586e
4eDo <61490588+4eDo@users.noreply.github.com>
2024-11-07 17:01:56 +0300

    Create navigation_generator.js

---
+++ navigation/navigation_generator.js
@@ -0,0 +1,73 @@
+function createTagList(data) {
+  const tagMap = {};
+  
+  data.forEach(item => {
+    item.tags.forEach(tag => {
+      const tagParts = tag.split(':').map(part => part.trim());
+      addToTagMap(tagMap, tagParts, item);
+    });
+  });
+  
+  const html = generateHTML(tagMap);
+  document.getElementById('sitemap_4eDo').innerHTML = html;
+}
+function addToTagMap(tagMap, tagParts, item) {
+  const tag = tagParts[0];
+  
+  if (!tagMap[tag]) {
+    tagMap[tag] = {
+      links: [],
+      subTags: {}
+    };
+  }
+  
+  if (tagParts.length > 1) {
+    const subTag = tagParts[1];
+    
+    if (!tagMap[tag].subTags[subTag]) {
+      tagMap[tag].subTags[subTag] = {
+        links: [],
+        subTags: {}
+      };
+    }
+    
+    addToTagMap(tagMap[tag].subTags[subTag].subTags, tagParts.slice(1), item);
+  } else {
+    if (!tagMap[tag].links.some(link => link.link === item.link)) {
+      tagMap[tag].links.push({
+        link: item.link,
+        title: item.title
+      });
+    }
+  }
+}
+function generateHTML(tagMap, isSubTag = false) {
+  let html = '';
+  
+  const sortedTags = Object.keys(tagMap).sort();
+  for (const tag of sortedTags) {
+    if (!isSubTag) {
+      html += `<ul class="tagList">`;
+      html += `<li class="tagTitle">${tag}</li>`;
+    } else {
+      html += `<ul class="tagList subList">`;
+      html += `<li class="tagList subTitle">${tag}</li>`;
+    }
+    
+    const sortedLinks = tagMap[tag].links.sort((a, b) => a.title.localeCompare(b.title));
+    
+    sortedLinks.forEach(linkObj => {
+      html += `<li><a class="tagItem" href="${linkObj.link}">${linkObj.title}</a></li>`;
+    });
+    
+    for (const subTag in tagMap[tag].subTags) {
+      const nestedHtml = generateHTML(tagMap[tag].subTags[subTag].subTags, true);
+      html += nestedHtml;
+    }
+    
+    html += `</ul>`;
+  }
+  
+  return html;
+}
+createTagList(LINKS);
