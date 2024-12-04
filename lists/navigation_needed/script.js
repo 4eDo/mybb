b9887cfde1cedebc297c9e8ee819ab70d567ce70
4eDo <61490588+4eDo@users.noreply.github.com>
2024-12-04 11:31:51 +0300

    Update script.js

--- lists/navigation_needed/script.js
+++ lists/navigation_needed/script.js
@@ -6,7 +6,7 @@ const TARGET_FORUMS = {
 
 console.group("Для Маяка от 4eDo");
 console.log("%c~~ Скрипт для автоматического ведения каталога заявок. %c https://github.com/4eDo ~~", "font-weight: bold;", "font-weight: bold;");
-console.log("v0.5");
+console.log("v0.6");
 console.groupEnd();
 /**
  * Выгрузка данных по темам
@@ -336,7 +336,9 @@ function searchInBlocks() {
     const matchesRelations = !relations || (ticket.relations.include.some(item => item.toLowerCase().includes(relations)) && !isBad(relations, ticket.relations.exclude));
     const matchesTags = !tags || (ticket.tags.include.some(item => item.toLowerCase().includes(tags)) && !isBad(tags, ticket.tags.exclude));
 
-    if (isBad(fandom, ticket.fandom.exclude) || isBad(setting, ticket.setting.exclude) || isBad(relations, ticket.relations.exclude) || isBad(tags, ticket.tags.exclude)) {
+    if (!fandom && !setting && !sex && !relations && !tags) {
+      details.classList.add('success');
+    } else if (isBad(fandom, ticket.fandom.exclude) || isBad(setting, ticket.setting.exclude) || isBad(relations, ticket.relations.exclude) || isBad(tags, ticket.tags.exclude)) {
       details.classList.add('bad');
     } else if (matchesFandom && matchesSetting && matchesSex && matchesRelations && matchesTags) {
       details.classList.add('success');
