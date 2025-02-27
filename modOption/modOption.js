7982e122a16f1b74234102c0f9e01812342ce9d8
4eDo <61490588+4eDo@users.noreply.github.com>
2025-02-27 14:56:47 +0300

    Update modOption.js

--- modOption/modOption.js
+++ modOption/modOption.js
@@ -1,18 +1,20 @@
+/** Модерирование тем: опция "Закрыть и перенести" от 4eDo */
 $(document).ready(function() {
+  if(GroupId < 3) {
     if (sessionStorage.getItem('returnPage')) {
-        sessionStorage.removeItem('returnPage');
-        window.location.href = $('#mod-options option:contains("Перенести тему")').val();
+      sessionStorage.removeItem('returnPage');
+      window.location.href = $('#mod-options option:contains("Перенести тему")').val();
     }
-  
     $('<option value="closeAndMove">Закрыть и перенести</option>')
       .insertAfter($('#mod-options option:contains("Модерирование темы")'));
     $('#mod-options').on('change', function() {
-        let selectedValue = $(this).val();
-        if (selectedValue === "closeAndMove") {
-            sessionStorage.setItem('returnPage', window.location.href);
-            window.location.href =  $('#mod-options option:contains("Закрыть тему")').val();
-        } else if (selectedValue) {
-            window.location.href = selectedValue;
-        }
+      let selectedValue = $(this).val();
+      if (selectedValue === "closeAndMove") {
+        sessionStorage.setItem('returnPage', window.location.href);
+        window.location.href =  $('#mod-options option:contains("Закрыть тему")').val();
+      } else if (selectedValue) {
+        window.location.href = selectedValue;
+      }
     });
+  }
 });
